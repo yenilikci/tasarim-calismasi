a745ed399c955166b957021a1c5e8862a3c19899
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-29 21:18:03 +0300

    dotenv config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -10,7 +12,9 @@ app.get('/', (req, res) => {
     res.json({status: 'success'})
 });
 
-app.listen('5000', (err) => {
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, (err) => {
     if (err) throw err;
     console.log('success')
 });
