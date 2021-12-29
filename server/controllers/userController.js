42528c1ad423b1ae35981f25265f03e4e5ae9e97
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-29 22:23:10 +0300

    register func modified

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,38 @@
-const register = (req, res) => {
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const register = asyncHandler(async (req, res) => {
     const {name, email, password, picture} = req.body;
-    res.json({
+
+    //user check
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        res.status(400);
+        throw new Error("User Already Exists");
+    }
+
+    //user create
+    const user = await User.create({
         name,
-        email
-    })
-};
+        email,
+        password,
+        picture
+    });
+
+    //success
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            picture: user.picture
+        })
+    } else { //error
+        res.status(400);
+        throw new Error("There Is An Error")
+    }
+
+});
 
 module.exports = {register}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,5 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const {mongoose} = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
