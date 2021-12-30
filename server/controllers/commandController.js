e92c0eb70b15e98ac85fdedd7d499120e4cabf47
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-30 16:13:19 +0300

    add update command by id

--- server/controllers/commandController.js
+++ server/controllers/commandController.js
@@ -29,4 +29,32 @@ const getCommandById = asyncHandler(async (req, res) => {
    }
 });
 
-module.exports = {getCommands, createCommand, getCommandById};
+const updateCommand = asyncHandler(async (req, res) => {
+    const {title, m1, m2, m3, m4, m5, m6, category} = req.body;
+
+    const command = await Command.findById(req.params.id);
+
+    if(command.user.toString() !== req.user._id.toString()){
+        res.status(401);
+        throw new Error("You Can't Perform This Action!");
+    }
+
+    if(command){
+        command.title = title;
+        command.m1 = m1;
+        command.m2 = m2;
+        command.m3 = m3;
+        command.m4 = m4;
+        command.m5 = m5;
+        command.m6 = m6;
+        command.category = category;
+
+        const updateCommand = await command.save();
+        res.json(updateCommand);
+    } else {
+        res.status(404);
+        throw new Error("Note Not Found!");
+    }
+});
+
+module.exports = {getCommands, createCommand, getCommandById, updateCommand};
--- server/routes/commandRoutes.js
+++ server/routes/commandRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getCommands, createCommand, getCommandById} = require('../controllers/commandController');
+const {getCommands, createCommand, getCommandById, updateCommand} = require('../controllers/commandController');
 const {protect} = require("../middlewares/authMiddleware");
 const {route} = require("express/lib/router");
 
 router.route("/").get(protect, getCommands);
 router.route("/create").post(protect, createCommand);
-router.route("/:id").get(getCommandById);
+router.route("/:id").get(getCommandById).put(protect, updateCommand);
 
 module.exports = router;
