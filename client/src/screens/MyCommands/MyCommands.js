48b15722ed6f526fa13dc78fa34f2d6ded7796a9
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-30 03:50:36 +0300

    add accordion feature added to command card

--- client/src/screens/MyCommands/MyCommands.js
+++ client/src/screens/MyCommands/MyCommands.js
@@ -1,13 +1,13 @@
 import Main from '../Main/Main'
 import {Link} from "react-router-dom";
-import {Badge, Button, Card, Table} from "react-bootstrap";
+import {Accordion, AccordionCollapse, Badge, Button, Card, Table} from "react-bootstrap";
 import "./myCommands.css"
 
 const MyCommands = () => {
 
     const deleteHandler = (id) => {
         if (window.confirm("Are you sure?")) {
-
+            //...
         }
     }
 
@@ -18,62 +18,73 @@ const MyCommands = () => {
                     Create New Command
                 </Button>
             </Link>
-            <Card className="bg-gradient border">
-                <Card.Header className="d-flex">
-                    <span className="title text-decoration-none">Simple Movement</span>
-                    <div>
-                        <Button
-                            href={`/editCommand/command._id`}
-                            variant="secondary">Edit</Button>
-                        <Button
-                            onClick={() => deleteHandler('command._id')}
-                            variant="danger" className="mx-1">
-                            Delete
-                        </Button>
-                    </div>
-                </Card.Header>
-                <Card.Body>
-                    <h4>
-                        <Badge variant="dark">
-                            Category: Default
-                        </Badge>
-                    </h4>
-                    <Table striped bordered hover>
-                        <thead className="text-center">
-                        <tr>
-                            <th>Degress</th>
-                            <th>Value</th>
-                        </tr>
-                        </thead>
-                        <tbody className="text-center">
-                        <tr>
-                            <td>M1</td>
-                            <td>0</td>
-                        </tr>
-                        <tr>
-                            <td>M2</td>
-                            <td>15</td>
-                        </tr>
-                        <tr>
-                            <td>M3</td>
-                            <td>180</td>
-                        </tr>
-                        <tr>
-                            <td>M4</td>
-                            <td>170</td>
-                        </tr>
-                        <tr>
-                            <td>M5</td>
-                            <td>0</td>
-                        </tr>
-                        <tr>
-                            <td>M6</td>
-                            <td>73</td>
-                        </tr>
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>
+            <Accordion className="my-1">
+                <Accordion.Item eventKey="0">
+                    <Card className="bg-gradient border">
+                        <Card.Header className="d-flex align-items-center">
+                        <span className="title text-decoration-none px-0">
+                            <Accordion.Header eventKey="0" as={Card.Text} className="shadow">
+                                <span className="text-dark">Simple Movement</span>
+                            </Accordion.Header>
+                        </span>
+                            <div>
+                                <Button
+                                    href={`/editCommand/command._id`}
+                                    variant="warning"
+                                    className="mx-1">Edit</Button>
+                                <Button
+                                    onClick={() => deleteHandler('command._id')}
+                                    variant="danger">
+                                    Delete
+                                </Button>
+                            </div>
+                        </Card.Header>
+                        <Accordion.Body>
+                            <Card.Body>
+                                <h4>
+                                    <Badge variant="dark">
+                                        Category: Default
+                                    </Badge>
+                                </h4>
+                                <Table striped bordered hover>
+                                    <thead className="text-center">
+                                    <tr>
+                                        <th>Degress</th>
+                                        <th>Value</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody className="text-center">
+                                    <tr>
+                                        <td>M1</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M2</td>
+                                        <td>15</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M3</td>
+                                        <td>180</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M4</td>
+                                        <td>170</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M5</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M6</td>
+                                        <td>73</td>
+                                    </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Accordion.Body>
+                    </Card>
+                </Accordion.Item>
+            </Accordion>
         </Main>
     )
 };
