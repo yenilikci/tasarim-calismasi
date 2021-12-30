6c38e5785707cc85730044476eee2c2e556f47ea
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-31 00:51:52 +0300

    add create command action

--- client/src/actions/commandsActions.js
+++ client/src/actions/commandsActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import {COMMANDS_LIST_FAIL, COMMANDS_LIST_REQUEST, COMMANDS_LIST_SUCCESS} from "../constants/commandsConstants";
+import {
+    COMMANDS_CREATE_FAIL,
+    COMMANDS_CREATE_REQUEST, COMMANDS_CREATE_SUCCESS,
+    COMMANDS_LIST_FAIL,
+    COMMANDS_LIST_REQUEST,
+    COMMANDS_LIST_SUCCESS
+} from "../constants/commandsConstants";
 
 export const listCommands = () => async (dispatch, getState) => {
     try {
@@ -34,6 +40,48 @@ export const listCommands = () => async (dispatch, getState) => {
                 : error.message;
         dispatch({
             type: COMMANDS_LIST_FAIL,
+            payload: message,
+        });
+    }
+};
+
+export const createCommandAction = (title, m1, m2, m3, m4, m5, m6, category) => async (
+    dispatch,
+    getState
+) => {
+    try {
+        dispatch({
+            type: COMMANDS_CREATE_REQUEST,
+        });
+
+        const {
+            userLogin: { userInfo },
+        } = getState();
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        const { data } = await axios.post(
+            `http://localhost:5000/api/v1/commands/create`,
+            { title, m1, m2, m3, m4, m5, m6, category },
+            config
+        );
+
+        dispatch({
+            type: COMMANDS_CREATE_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        const message =
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message;
+        dispatch({
+            type: COMMANDS_CREATE_FAIL,
             payload: message,
         });
     }
