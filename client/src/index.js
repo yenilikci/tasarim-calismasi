d4f18d9343f4f8fb8b7adbdd4b533bd73fe1a4d7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-30 07:21:58 +0300

    add redux, react-redux, redux-thunk and creating store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap.min.css'
+import './assets/bootstrap.min.css'
 import App from './App';
+import {Provider} from "react-redux";
+import store from "./store"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
