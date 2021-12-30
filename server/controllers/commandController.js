19bd2567ec8dfe9030753d0bdff7c60ee899f345
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-30 15:54:13 +0300

    add create command endpoint, createCommand func

--- server/controllers/commandController.js
+++ server/controllers/commandController.js
@@ -1,9 +1,22 @@
 const Command = require('../models/commandModel');
 const asyncHandler = require("express-async-handler");
 
-const getCommands = asyncHandler(async (req,res) => {
-    const commands = await Command.find();
+const getCommands = asyncHandler(async (req, res) => {
+    const commands = await Command.find({user: req.user._id});
     res.json(commands);
 });
 
-module.exports = {getCommands};
+const createCommand = asyncHandler(async (req, res) => {
+    const {title, m1, m2, m3, m4, m5, m6, category} = req.body;
+
+    if (!title || !m1 || !m2 || !m3 || !m4 || !m5 || !m6 || !category) {
+        res.status(400);
+        throw new Error("Please Fill All The Fields");
+    } else {
+        const command = new Command({user: req.user._id, title, m1, m2, m3, m4, m5, m6, category});
+        const createdCommand = await command.save();
+        res.status(201).json(createdCommand);
+    }
+})
+
+module.exports = {getCommands, createCommand};
--- server/routes/commandRoutes.js
+++ server/routes/commandRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getCommands} = require('../controllers/commandController');
+const {getCommands, createCommand} = require('../controllers/commandController');
 const {protect} = require("../middlewares/authMiddleware");
 
 router.route("/").get(protect, getCommands);
+router.route("/create").post(protect, createCommand);
 
 module.exports = router;
