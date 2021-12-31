31f84bfe25b3008c233b282f0ee6f3905a51c536
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-12-31 20:44:34 +0300

    modified header log in&out

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import {Navbar, Nav, Container, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
 import './header.css';
-import {Link, useHistory} from "react-router-dom";
+import {Link, useHistory, useLocation} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {logout} from "../../actions/userActions";
 import {GiRobotGrab} from "react-icons/gi";
@@ -10,18 +10,24 @@ const Header = ({setSearch}) => {
 
     const history = useHistory();
 
+    const location = useLocation();
+
     const dispatch = useDispatch();
 
     const userLogin = useSelector(state => state.userLogin);
-
     const {userInfo} = userLogin;
 
     const logoutHandler = () => {
         dispatch(logout());
         history.push('/');
+        window.location.reload();
     }
 
     useEffect(() => {}, [userInfo]);
+
+    useEffect(()=> {
+        console.log('log: ' + userInfo)
+    },[]);
 
     return (
         <Navbar expand="lg" className="d-flex my-custom-navbar">
@@ -31,34 +37,36 @@ const Header = ({setSearch}) => {
                     Robotic Arm Control
                 </Link>
             </Navbar.Brand>
-            <Navbar.Brand>
-                <Form className="d-flex">
-                    <FormControl
-                        type="search"
-                        placeholder="Search..."
-                        className="border border-5"
-                        aria-label="Search"
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
-                </Form>
-            </Navbar.Brand>
+            {
+                userInfo !== null ? ( <Navbar.Brand>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search..."
+                            className="border border-5"
+                            aria-label="Search"
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </Form>
+                </Navbar.Brand>) : null
+            }
             {/*<Nav className="d-flex justify-content-between">*/}
             {/*    <Nav.Link href="">*/}
             {/*        <Button variant="primary border border-3 border-dark" size="md">About</Button>*/}
             {/*    </Nav.Link>*/}
             {/*</Nav>*/}
-            <NavDropdown title="Menu" className="my-dropdown">
+            {userInfo !== null ? (<NavDropdown title="Menu" className="my-dropdown">
                 <NavDropdown.Item
                     href="/myCommands"
                     className="my-dropdown-item">
-                   My Commands
+                    My Commands
                 </NavDropdown.Item>
                 <NavDropdown.Item
                     onClick={logoutHandler}
                 >
                     Logout
                 </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown>) : null}
         </Navbar>
     )
 }
